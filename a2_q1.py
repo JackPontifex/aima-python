11abd8e3f62835587b68d0c6abac49436e46ca57
jpontife <jpontife@sfu.ca>
2019-05-25 13:59:45 -0700

    question 1

--- a2_q1.py
+++ a2_q1.py
@@ -1,1 +1,19 @@
 # Question 1 code
+import random
+
+def decision(probability):
+  return random.random() < probability
+
+def rand_graph(n, p):
+  randomGraph = {}
+  print(decision(0.5))
+  for i in range(0, n):
+    randomGraph[i] = []
+
+  for i in range(0, n):
+    for j in range(i + 1, n):
+      if decision(p):
+        randomGraph[i].append(j)
+        randomGraph[j].append(i)
+
+  return randomGraph
